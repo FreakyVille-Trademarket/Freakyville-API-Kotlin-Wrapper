9b925db6d6159d2123aab206a5ec5a2f5a72ce1e
WildTooth <champenv@outlook.dk>
2024-03-25 16:01:34 +0100

    Added documentation for WheelWrapper.kt

--- src/main/kotlin/dk/fvtrademarket/api/kt/wheel/WheelWrapper.kt
+++ src/main/kotlin/dk/fvtrademarket/api/kt/wheel/WheelWrapper.kt
@@ -2,23 +2,42 @@ package dk.fvtrademarket.api.kt.wheel
 
 import java.util.UUID
 
+/**
+ * Represents a spinny wheel from the Freakyville website.
+ * This class is used to represent the response from the Freakyville API when requesting a wheel.
+ *
+ * @param success Whether the request was successful.
+ * @param wheel The wheel.
+ */
 class WheelWrapper(success: Boolean, private val wheel: Wheel) {
     init {
         require(success) { "WheelWrapper must have success=true" }
     }
 
+    /**
+     * @return The ID of the wheel as a UUID.
+     */
     fun getID(): UUID {
         return UUID.fromString(wheel.id)
     }
 
+    /**
+     * @return The options of the wheel.
+     */
     fun getOptions(): Array<String> {
         return wheel.options.map { it.option }.toTypedArray()
     }
 
+    /**
+     * @return The winning option as it is shown on the wheel.
+     */
     fun getWinner(): String {
         return wheel.options[wheel.winner.toInt()].option
     }
 
+    /**
+     * @return The time the wheel was created.
+     */
     fun getCreatedAt(): String {
         return wheel.createdAt
     }
@@ -32,6 +51,14 @@ class WheelWrapper(success: Boolean, private val wheel: Wheel) {
         return wheel
     }
 
+    /**
+     * Represents the spinny wheel from the request.
+     *
+     * @param id The ID of the wheel.
+     * @param options The options of the wheel.
+     * @param winner The winner of the wheel.
+     * @param createdAt The time the wheel was created.
+     */
     data class Wheel(val id: String, val options: Array<Option>, val winner: String, val createdAt: String) {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
@@ -56,5 +83,9 @@ class WheelWrapper(success: Boolean, private val wheel: Wheel) {
         }
     }
 
+    /**
+     * Represents an option on the wheel.
+     * It is only needed to represent the response from the Freakyville API.
+     */
     data class Option(val option: String)
 }
